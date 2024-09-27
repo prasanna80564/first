1d1d7f57cec093ddadc728ba0696ae108211892d
prasanna80564 <20039859@mydbs.ie>
2024-09-27 14:31:22 +0100

    update

--- euler.js
+++ euler.js
@@ -1,23 +1,23 @@
 
-    alert ("loading euler.js");
+alert ("loading euler.js");
 
-    let a = 3;
-    let b = 5;
-    let n = 1000;
-    let sum = 0;
+let a = 3;
+let b = 5;
+let n = 1000;
+let sum = 0;
 
-    for (let i = 1; i < n; i++) {
-    if (i % a === 0 || i % b === 0) {
-        sum += i; 
-        }
+for (let i = 1; i < n; i++) {
+if (i % a === 0 || i % b === 0) {
+    sum += i; 
     }
+}
 
-    alert("The sum of all multiples :" + sum);
+alert("The sum of all multiples :" + sum);
 
 
-        
-            
     
+        
+
 
 
 
