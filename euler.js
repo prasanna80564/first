236f9b852e42c893bbfebf273d4553915d2248a1
prasanna80564 <20039859@mydbs.ie>
2024-09-27 14:08:35 +0100

    update

--- euler.js
+++ euler.js
@@ -9,7 +9,7 @@
             let sum =0;
              
             
-            for (let i = 1; i < n; i++) {
+            for (let i = 1;i<n;i++) {
             if (i % a === 0 || i % b === 0) {
                 sum += i;
             }
