2a4637fd6f78daabebb2e98eb4d70b4e21ae73ee
prasanna80564 <20039859@mydbs.ie>
2024-09-29 14:39:07 +0100

    update

--- f.js
+++ f.js
@@ -1,23 +1,11 @@
-let a=3;
-let b=5;
-let n=1000;
-let sum = 0;
-
-function sumofmultiplies(){
-   
-   
-
-    for (let i = 1;i>n;i++){
-        if(i%a === 0 || i%b === 0){
+function sumOfMultiples(a, b, n) {
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        if (i % a === 0 || i % b === 0) {
             sum += i;
         }
     }
-    alert("The sum of all multiples :" + sum);
+    return sum;
 }
 
- sumofmultiplies(a,b,n);  
-   
-
-    
-
-
+alert(sumOfMultiples(3, 5, 10))
