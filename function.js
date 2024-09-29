b5092997cb0009a7bd00ed2759492e16b0d8f49c
prasanna80564 <20039859@mydbs.ie>
2024-09-29 14:28:07 +0100

    alert

--- function.js
+++ function.js
@@ -1,8 +1,10 @@
+let a=3;
+let b=5;
+let n=1000;
+
 function sumofmultiplies(){
     let sum = 0;
-    let a=3;
-    let b=5;
-    let n=1000;
+   
 
     for (let i = 1;i>n;i++){
         if(i%a === 0 || i%b === 0){
@@ -13,7 +15,7 @@ function sumofmultiplies(){
 }
 
     let result = sumofmultiplies(a,b,n);  
-    console.log (result);
+    alert(result);
 
     
 
