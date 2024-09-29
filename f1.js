e5a730ba29db9ed02580bce0d125e4b3c6a042b4
prasanna80564 <20039859@mydbs.ie>
2024-09-29 15:19:20 +0100

    update

--- f1.js
+++ f1.js
@@ -1,13 +1,16 @@
-function calculateSum (a, b, n) {
+function calculateSum (num) {
     const num = parseInt(document.getElementById("numberInput").value);
     let sum = 0;
-    let n = 10;
-    for (let i = 0; i < n; i++) {
+    let a = 3;
+    let b = 5;
+
+  
+    for (let i = 0; i < num; i++) {
         if (i % a === 0 || i % b === 0) {
             sum += i;
         }
     }
     
-    alert("The sum of multiples of 3 or 5 below " + n + " is: " + sum)
+    alert("The sum of multiples of 3 or 5 below " + num + " is: " + sum)
 }
 
