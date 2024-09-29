5a8db6914b6866cb23de172f1dc1b8df553a5fdc
prasanna80564 <20039859@mydbs.ie>
2024-09-29 15:17:02 +0100

    update

--- f1.js
+++ f1.js
@@ -1,6 +1,7 @@
 function calculateSum (a, b, n) {
     const num = parseInt(document.getElementById("numberInput").value);
     let sum = 0;
+    let n = 10;
     for (let i = 0; i < n; i++) {
         if (i % a === 0 || i % b === 0) {
             sum += i;
