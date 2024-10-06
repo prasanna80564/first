a3ca76f2839c162d2b976e5ab6e4df1f766948d4
prasanna80564 <20039859@mydbs.ie>
2024-10-06 14:20:30 +0100

    push

---
+++ array1.js
@@ -0,0 +1,14 @@
+function sumofmultiple (a,l){
+    let sum = 0;
+    for (let i=0;i<l.length;i++ ) {
+        if(l[i]%a[0]===0 || l[i]%a[1]===0) {
+            sum =+ l[i];
+        }
+    }
+    return sum;
+
+}
+
+let a = [2,3];
+let l = [1,2,3,4,5,6,7,8,9,10];
+alert(sumofmultiple(a,l));
