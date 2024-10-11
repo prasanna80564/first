050e693989b12c971d3737085bcf6e5479ee9dd5
prasanna80564 <20039859@mydbs.ie>
2024-10-11 13:14:11 +0100

    rename

--- bucket.js
+++ bucket.js
@@ -1,4 +1,4 @@
-let f1 = (basket,prices)=>{
+let b = (basket,prices)=>{
     let totalCost = 0;
     for(let item in basket){
         if(prices[item] != undefined){
@@ -9,4 +9,4 @@ let f1 = (basket,prices)=>{
 }
 basket = {apple:1,orange:1,grapes:5};
 price = {apple:20,orange:12,grapes:22,tea:5,milk:10};
-alert(f1(basket,price));
+alert(b(basket,price));
